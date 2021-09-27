a0c145f711df11016469ef613e09f7a6389c4f66
Bryan Berry <bberry@octavebio.com>
2021-09-26 17:42:26 -0700

    bioinf_0: Add 1.2

---
+++ coursera/bioinf_0/1.2/get_ori.py
@@ -0,0 +1,19 @@
+import urllib3
+
+# First, create a string variable called ori that is equal to the Vibrio cholerae ori. Don't forget to enclose your string in quotes!
+
+url = "https://bioinformaticsalgorithms.com/data/realdatasets/Replication/v_cholerae_oric.txt"
+http = urllib3.PoolManager()
+
+print("INFO: Fetching string ori from url: {url}...")
+try:
+    r = http.request('GET', url)
+    if r.status == 200:
+      ori_raw = r.data.decode('utf-8')
+      print(ori_raw)
+except:
+    print("ERROR: Failed to GET ori from url: {url}")
+    exit(1)
+
+# Then, print the length of ori
+print("INFO: ori length: {}".format(len(ori_raw)))

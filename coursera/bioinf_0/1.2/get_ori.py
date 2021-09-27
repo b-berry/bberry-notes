1513c32e564a9e1afd90e3c1557d67c5441aaad4
Bryan Berry <bberry@octavebio.com>
2021-09-27 08:56:55 -0700

    bioninf_0: Clean script for expected basic stdout

--- coursera/bioinf_0/1.2/get_ori.py
+++ coursera/bioinf_0/1.2/get_ori.py
@@ -1,5 +1,19 @@
 import urllib3
 
+url = "https://bioinformaticsalgorithms.com/data/realdatasets/Replication/v_cholerae_oric.txt"
+
+def GetOri(url):
+    http = urllib3.PoolManager()
+    print("INFO: Fetching string ori from url: {url}...")
+    try:
+        r = http.request('GET', url, timeout=6.0)
+        if r.status == 200:
+          ori_raw = r.data.decode('utf-8')
+          return ori_raw
+    except urllib3.exceptions.NewConnectionError:
+        print("ERROR: Failed to GET ori from url: {url}")
+        exit(1)
+
 def PatternCount(Text, Pattern):
     print("INFO: Searching for k-mer: {}".format(Pattern))
     count = 0
@@ -9,24 +23,15 @@ def PatternCount(Text, Pattern):
     return count
 
 # First, create a string variable called ori that is equal to the Vibrio cholerae ori. Don't forget to enclose your string in quotes!
-url = "https://bioinformaticsalgorithms.com/data/realdatasets/Replication/v_cholerae_oric.txt"
-http = urllib3.PoolManager()
-
-print("INFO: Fetching string ori from url: {url}...")
-try:
-    r = http.request('GET', url, timeout=6.0)
-    if r.status == 200:
-      ori_raw = r.data.decode('utf-8')
-      print(ori_raw)
-except urllib3.exceptions.NewConnectionError:
-    print("ERROR: Failed to GET ori from url: {url}")
-    exit(1)
+ori_raw = "ATCAATGATCAACGTAAGCTTCTAAGCATGATCAAGGTGCTCACACAGTTTATCCACAACCTGAGTGGATGACATCAAGATAGGTCGTTGTATCTCCTTCCTCTCGTACTCTCATGACCACGGAAAGATGATCAAGAGAGGATGATTTCTTGGCCATATCGCAATGAATACTTGTGACTTGTGCTTCCAATTGACATCTTCAGCGCCATATTGCGCTGGCCAAGGTGACGGAGCGGGATTACGAAAGCATGATCATGGCTGTTGTTCTGTTTATCTTGTTTTGACTGAGACTTGTTAGGATAGACGGTTTTTCATCACTGACTAGCCAAAGCCTTACTCTGCCTGACATCGACCGTAAATTGATAATGAATTTACATGCTTCCGCGACGATTTACCTCTTGATCATCGATCCGATTGAAGATCTTCAATTGTTAATTCTCTTGCCTCGACTCATAGCCATGATGAGCTCTTGATCATGTTTCCTTAACCCTCTATTTTTTACGGAAGAATGATCAAGCTGCTGCTCTTGATCATCGTTTC"
 
 # Then, print the length of ori
-print("INFO: ori length: {}".format(len(ori_raw)))
+#print("INFO: ori length: {}".format(len(ori_raw)))
+print(len(ori_raw))
 
 # Then print  the number of times that the string Pattern = "TGATCA" occurs in the string Text corresponding to the Vibrio cholerae ori
 Pattern = "TGATCA"
 
 result = PatternCount(ori_raw, Pattern)
-print("INFO: ori contains: {} instances of {} k-mer".format(result, Pattern))
+#print("INFO: ori contains: {} instances of {} k-mer".format(result, Pattern))
+print(result)
